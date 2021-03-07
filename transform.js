0a86fbf80202162d190416d19cfd4560676a0df6
Julian van Dijk <j.m.c.vandijk@student.tudelft.nl>
2021-03-07 21:55:26 +0100

    btingo :

---
+++ transform.js
@@ -0,0 +1,27 @@
+`my internet lost connection
+someone talks while muted
+can you hear me
+someone is too late
+accidental noise
+how does screen sharing works
+roommate walks in
+screen freezes
+someones cat walks in front of the camera
+awkward waving
+hear someone typing
+someone says vo on bt
+julians camera is black and white
+guus freezes
+the video a company wants to show doesnt work
+‘breakout rooms are opened’
+someone yawns on camera
+‘i am unable to enter the room’
+kilian has a question
+somebody eats in front of the camera
+somebody makes a corona joke
+people are sitting together behind their camera
+somebody drinks from the adyen water bottle
+somebody drinks from the BT coffe cup
+`.split('\n').forEach((x, i)=> {
+    console.log(`\"${i}\" :\"${x}\",`)
+})
